8c3727719f1906945d601a3cde5555eb1b3d29ff
Alexis Foulont <alexisfoulont@gmail.com>
2023-02-25 16:48:30 +0100

    create modal file

---
+++ frontend/src/modal/AsoiafModal/AsoiafModal.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/destructuring-assignment */
+import { ImCross } from "react-icons/im";
+import PropTypes from "prop-types";
+import "./style.scss";
+
+export default function AsoiafModal(props) {
+  if (!props.show) {
+    return null;
+  }
+
+  return (
+    <div className="modal">
+      <div className="modalcontent">
+        <h1 className="modalTitle">So, what do you have to do ?</h1>
+      </div>
+      <div className="modalBody">
+        <ul className="firstBlock">
+          <li className="firstInstruc">
+            Choose the number of players by clicking on
+          </li>
+          <li className="SecondInstruc">
+            Insert all players'names, validate and click on the ready button
+          </li>
+          <li className="ThirdInstruc">Choose your name and off you go !</li>
+        </ul>
+        <p className="part1">
+          You will have 20 seconds to find the maximum of quotes.
+        </p>
+        <p className="part3">
+          Choose the right answer:
+          <br />
+          The faster you are, the more points you get !
+        </p>
+        <p className="quoteJoke">
+          "And may the odds be ever in your favor" <br />
+          ...Nah ! that's a movie quote ! wrong game !
+        </p>
+      </div>
+      <div className="modal-footer">
+        <button type="button" onClick={props.onClose} className="buttonClose">
+          <ImCross className="closeIcon pulse" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+AsoiafModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
--- frontend/src/pages/Asoiaf/Asoiaf.jsx
+++ frontend/src/pages/Asoiaf/Asoiaf.jsx
@@ -1,13 +1,26 @@
 import Navbar from "@components/Navbar/Navbar";
 import Neutral from "@components/Neutral/Neutral";
 import Targaryen from "@components/Targaryen/Targaryen";
+// import { useState } from "react";
+// import AsoiafModal from "src/modal/AsoiafModal/AsoiafModal";
 import "./style.scss";
 
 export default function Asoiaf() {
+  // const [show, setShow] = useState(false);
   return (
     <section className="asoiafPage">
       <Navbar />
       <h1 className="titlePage">A Song Of Ice & Fire - Miniatures</h1>
+      <div className="asoiafModal">
+        <button
+          type="button"
+          className="instrucButton"
+          // onClick={() => setShow(true)}
+        >
+          En savoir plus sur ce jeu
+        </button>
+        {/* <AsoiafModal onClose={() => setShow(false)} show={show} /> */}
+      </div>
       <section className="Gallery">
         <div className="targaryen">
           <Targaryen />
